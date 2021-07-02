e0de87e1754a58841a934429cabc01616e2b0400
Spirit1230 <tom.osborn1@btinternet.com>
2021-07-02 11:38:12 +0100

    Moved code for running programme

---
+++ CoreProgramme.py
@@ -0,0 +1,25 @@
+import Inventory
+
+import Inventory
+
+if __name__ == "__main__" :
+    inventoryName = "StoreInventory.csv"
+
+    storeInventory = Inventory.Inventory(inventoryName)
+
+    accessingInventory = True
+
+    while accessingInventory :
+        storeInventory.DisplayOptions()
+        optionPicked = str(input("Please pick an option : "))
+
+        if (optionPicked == "1") :
+            storeInventory.SearchItem()
+        elif (optionPicked == "2") :
+            storeInventory.AddItem()
+        elif (optionPicked == "3") :
+            storeInventory.RemoveItem()
+        elif (optionPicked == "4") :
+            accessingInventory = False
+        else :
+            print("Invalid input")
