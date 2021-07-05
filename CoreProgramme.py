cb1c39667f1f55763c819ed86772a9a334cf1e1f
Spirit1230 <tom.osborn1@btinternet.com>
2021-07-05 13:08:35 +0100

    Updated to test checkout functions

--- CoreProgramme.py
+++ CoreProgramme.py
@@ -1,29 +1,43 @@
+import Checkout
 import Inventory
 
 if __name__ == "__main__" :
 
-    #creates instance of Inventory() class
+    print("Do you want to test :\n1. Inventory Controls\n2. Checkout")
+    choice = str(input())
+
     inventoryName = "StoreInventory.csv"
-    storeInventory = Inventory.Inventory(inventoryName)
 
-    accessingInventory = True
+    if (choice == "1") :
+        #creates instance of Inventory() class
+        storeInventory = Inventory.Inventory(inventoryName)
 
-    #interface for interacting with the Inventory() class
-    while accessingInventory :
-        storeInventory.DisplayOptions()
-        optionPicked = str(input("Please pick an option : "))
+        accessingInventory = True
 
-        if (optionPicked == "1") :
-            storeInventory.SearchItem()
-        elif (optionPicked == "2") :
-            storeInventory.AddItem()
-        elif (optionPicked == "3") :
-            storeInventory.RemoveItem()
-        elif (optionPicked == "4") :
-            storeInventory.AddStock()
-        elif (optionPicked == "5") :
-            storeInventory.RemoveStock()
-        elif (optionPicked == "6") :
-            accessingInventory = False
-        else :
-            print("Invalid input")
+        #interface for interacting with the Inventory() class
+        while accessingInventory :
+            storeInventory.DisplayOptions()
+            optionPicked = str(input("Please pick an option : "))
+
+            if (optionPicked == "1") :
+                storeInventory.SearchItem()
+            elif (optionPicked == "2") :
+                storeInventory.AddItem()
+            elif (optionPicked == "3") :
+                storeInventory.RemoveItem()
+            elif (optionPicked == "4") :
+                storeInventory.AddStock()
+            elif (optionPicked == "5") :
+                storeInventory.RemoveStock()
+            elif (optionPicked == "6") :
+                accessingInventory = False
+            else :
+                print("Invalid input")
+    elif (choice == "2") :
+        checkout = Checkout.Checkout(inventoryName)
+
+        for num in range(0, 5) :
+            checkout.ScanItem()
+        checkout.PrintReceipt()
+    else :
+        print("Invalid input")
